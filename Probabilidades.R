28e7bf07b2f35336029f1109b47ffcc6dfd792f1
slandazuri <152200572+slandazuri@users.noreply.github.com>
2024-02-28 11:32:35 -0500

    permutaciones y combinaciones

--- Probabilidades.R
+++ Probabilidades.R
@@ -1,13 +1,55 @@
 vector <- 
-c(12, 13, 7, 15, 20, 18)
+c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 permutacion <- 
   function(vector, eventos){
+    
 longitud <- length(vector)
-return(permutations(n = longitud, 
-                    r = eventos))
+return(
+  permutations(
+    n = longitud, 
+    r = eventos, 
+    v = vector,
+    repeats.allowed = FALSE)
+  )
+  }
+
+numeroPermutaciones 
+<- function(vector, eventos){
+  n <- length(vector)
+  
+return(factorial(n) / factorial(n - eventos))  
 }
 
-permutacion(vector, 2)  
+permutacion(vector, 3)
+numeroPermutaciones(vector, 3)
+
+
+combinaciones <- function(vector, eventos){
+  longitud <- length(vector)
+  return(combinations(
+          n = longitud,
+          r = eventos,
+          v = vector,
+          repeats.allowed = TRUE)
+        )  
+}
+
+numeroCombinaciones <- function(vector, eventos){
+  n <- length(vector)
+  nr <- n - eventos
   
-  
+return(
+    factorial(n) / 
+    (factorial(nr) * factorial(eventos))
+    )
+}
+
+
+combinaciones(vector, 3)
+numeroCombinaciones(vector, 3)
+
+
+
+
+
